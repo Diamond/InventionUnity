a82b4fa86dee3be22f736553b378fe7eea39d04f
Brandon Richey <diamondgfx@gmail.com>
2014-08-14 00:03:06 -0400

    Project 2 is underway

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	public bool walking = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Get the Animator component from your gameObject
+		Animator anim = GetComponent<Animator>();
+		// Sets the value
+		anim.SetBool("Walking", true); 
+
+		Vector3 moving = new Vector3(1.0f, this.rigidbody2D.velocity.y, 0.0f);
+		this.rigidbody2D.velocity = moving;
+	}
+}
